183fc0d5ff8e4e41050a9b22daff2b7a3c9abdab
Pavel Vasilev <pavel.v96@yandex.com>
2019-06-21 22:53:36 +0300

    fix navigation from within withState function

--- app/src/main/java/com/pvasilev/aviasales/MainActivity.kt
+++ app/src/main/java/com/pvasilev/aviasales/MainActivity.kt
@@ -1,22 +1,33 @@
 package com.pvasilev.aviasales
 
 import android.os.Bundle
+import android.os.Looper
 import com.airbnb.mvrx.BaseMvRxActivity
 import com.pvasilev.aviasales.presentation.OnBackPressedListener
 import com.pvasilev.aviasales.presentation.inject
 import com.pvasilev.aviasales.presentation.location.LocationScreen
-import com.pvasilev.aviasales.presentation.location.LocationViewModel
 import ru.terrakok.cicerone.Navigator
 import ru.terrakok.cicerone.NavigatorHolder
 import ru.terrakok.cicerone.Router
 import ru.terrakok.cicerone.android.support.SupportAppNavigator
+import ru.terrakok.cicerone.commands.Command
 
 class MainActivity : BaseMvRxActivity() {
     private val navigatorHolder: NavigatorHolder by inject("AppScope")
 
     private val router: Router by inject("AppScope")
 
-    private val navigator: Navigator = SupportAppNavigator(this, R.id.container)
+    private val navigator: Navigator = object : SupportAppNavigator(this, R.id.container) {
+        override fun applyCommands(commands: Array<out Command>?) {
+            if (Thread.currentThread() != Looper.getMainLooper().thread) {
+                runOnUiThread {
+                    super.applyCommands(commands)
+                }
+            } else {
+                super.applyCommands(commands)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
