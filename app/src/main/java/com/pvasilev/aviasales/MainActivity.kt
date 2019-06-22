d38c9bb2b284b61b614d8c763d57d47083a681c3
Pavel Vasilev <pavel.v96@yandex.com>
2019-06-22 13:49:49 +0300

    add custom enter exit animation

--- app/src/main/java/com/pvasilev/aviasales/MainActivity.kt
+++ app/src/main/java/com/pvasilev/aviasales/MainActivity.kt
@@ -2,10 +2,15 @@ package com.pvasilev.aviasales
 
 import android.os.Bundle
 import android.os.Looper
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.airbnb.mvrx.BaseMvRxActivity
 import com.pvasilev.aviasales.presentation.OnBackPressedListener
 import com.pvasilev.aviasales.presentation.inject
+import com.pvasilev.aviasales.presentation.location.LocationFragment
 import com.pvasilev.aviasales.presentation.location.LocationScreen
+import com.pvasilev.aviasales.presentation.map.MapFragment
+import com.pvasilev.aviasales.presentation.search.SearchFragment
 import ru.terrakok.cicerone.Navigator
 import ru.terrakok.cicerone.NavigatorHolder
 import ru.terrakok.cicerone.Router
@@ -25,6 +30,32 @@ class MainActivity : BaseMvRxActivity() {
                 }
             } else {
                 super.applyCommands(commands)
+            }
+        }
+
+        override fun setupFragmentTransaction(
+            command: Command?,
+            currentFragment: Fragment?,
+            nextFragment: Fragment?,
+            fragmentTransaction: FragmentTransaction?
+        ) {
+            if (currentFragment is LocationFragment) {
+                if (nextFragment is SearchFragment) {
+                    fragmentTransaction?.setCustomAnimations(
+                        R.anim.slide_up_enter,
+                        R.anim.slide_up_exit,
+                        R.anim.slide_down_pop_enter,
+                        R.anim.slide_down_pop_exit
+                    )
+                }
+                if (nextFragment is MapFragment) {
+                    fragmentTransaction?.setCustomAnimations(
+                        R.anim.slide_left_enter,
+                        R.anim.slide_left_exit,
+                        R.anim.slide_right_pop_enter,
+                        R.anim.slide_right_pop_exit
+                    )
+                }
             }
         }
     }
